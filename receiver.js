cbad2f3e10bcad3f243947a81040b31b8b507262
Uli Dambeck <ud@golinet.de>
2023-07-17 13:46:46 +0200

    .

--- receiver.js
+++ receiver.js
@@ -126,8 +126,8 @@ app.post(config.sender.post.path, function (req, res) {
   }
   for (let i = 0; i < dbstructure.files.length; i++) {
     const element = dbstructure.files[i];
+    dumpMsg(`data[${element.name}] = ${data[element.name]}`);
     if (data[element.name] != '') {
-      dumpMsg(`data[${element.name}] = ${data[element.name]}`);
       let _path = config.receiver.store + element.name;
       let elData = new Buffer.from(data[element.name], 'binary');
       dumpMsg(' - Checking Basepath: ' + _path);
